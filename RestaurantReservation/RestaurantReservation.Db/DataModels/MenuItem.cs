16b8de3d1a67b0804821e9cff133db3c77a04fe4
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-10-11 22:27:07 +0300

    Create Data Models for DBcontext

---
+++ RestaurantReservation/RestaurantReservation.Db/DataModels/MenuItem.cs
@@ -0,0 +1,14 @@
+﻿namespace RestaurantReservation.Db.DataModels
+{
+    public class MenuItem
+    {
+        public int ItemId { get; set; }
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+
+        public Restaurant Restaurant { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+}
---
+++ RestaurantReservation/RestaurantReservation.Db/DataModels/OrderItem.cs
@@ -0,0 +1,12 @@
+﻿namespace RestaurantReservation.Db.DataModels
+{
+    public class OrderItem
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+        public Order Order { get; set; }
+        public MenuItem MenuItem { get; set; }
+    }
+}
